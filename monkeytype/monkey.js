7d3a69ad34e5051eec1c065158fe5bb3b614d5fd
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 12:24:19 -0400

    test

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -3,11 +3,12 @@ fetch('monkeytype/1-1000.txt')
     .then(response => response.text())
     .then(data => words = data);
 
-let wordlibrary = words.split(" ")
-console.log(wordlibrary.slice(0, 50))
+console.log(typeof words);
+let wordlibrary = words.split(" ");
+console.log(wordlibrary.slice(0, 50));
 document.getElementById("words").innerHTML = wordlibrary;
 
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
