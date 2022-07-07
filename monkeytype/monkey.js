1e94fdabbf77731efdbb84c09d388ca721210688
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 12:48:02 -0400

    changed variables

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,7 +1,6 @@
 var words
-fetch('monkeytype/1-1000.txt')
-    .then(response => response.text())
-    .then(data => words = String(data));
+let fetchwords = fetch('monkeytype/1-1000.txt');
+fetchwords.then(response => response.text()).then(data => words = String(data));
 
 console.log(typeof words);
 console.log(words);
