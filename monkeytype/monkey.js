2c565b8fb922fa0da92f825471770419582fcb79
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 13:14:14 -0400

    string

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,7 +1,7 @@
 var wordlibrary;
 fetch('monkeytype/1-1000.txt')
     .then(response => response.text())
-    .then(data => wordlibrary = data.split('\n'));
+    .then(data => wordlibrary = String(data.split('\n')));
 
 console.log(typeof wordlibrary);
 console.log(wordlibrary.slice(0, 50));
