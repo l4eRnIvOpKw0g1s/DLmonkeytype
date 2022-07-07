3c53443f37c940c22ab8fa8c58192bdbc0968523
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 03:41:59 -0400

    print

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,4 +1,4 @@
 console.log("hello")
 fetch('monkeytype/1-1000.txt')
     .then(response => response.text())
-    .then(data => console.log(data));
+    .then(data => document.getElementById("words").innerHTML = (data));
