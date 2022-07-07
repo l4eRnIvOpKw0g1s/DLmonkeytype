b5cdbdf89c103a9acce9f2cc984d8a7f86779a15
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 13:03:41 -0400

    changed

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,8 +1,12 @@
-var fs = require('fs');
-var text = fs.readFileSync("monkeytype/1-1000.txt", 'utf-8');
-var textByLine = text.split('\n');
-console.log (typof (textByLine));
-console.log (textByLine);
+let words;
+fetch('monkeytype/1-1000.txt')
+    .then(response => response.text())
+    .then(data => wordlibrary = data.split('\n'));
+
+console.log(typeof wordlibrary);
+console.log(wordlibrary.slice(0, 50));
+document.getElementById("words").innerHTML = wordlibrary;
+
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
