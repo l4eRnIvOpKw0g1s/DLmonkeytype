8d429c547cbc28ee2634ceb66c6c46bfe6e6f68c
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 12:51:54 -0400

    trying again

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,13 +1,8 @@
-var words
-let fetchwords = fetch('monkeytype/1-1000.txt');
-fetchwords.then(response => response.text()).then(data => words = String(data));
-
-console.log(typeof words);
-console.log(words);
-let wordlibrary = words.split(" ");
-console.log(wordlibrary.slice(0, 50));
-document.getElementById("words").innerHTML = wordlibrary;
-
+var fs = require('fs');
+var text = fs.readFileSync("monkeytype/1-1000.txt", 'utf-8');
+var textByLine = text.split('\n');
+console.log (typof textByLine);
+console.log (textByLine);
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
