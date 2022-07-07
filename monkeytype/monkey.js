7de4a5b471d313bd7ef66d9266a521047bd73118
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 12:17:44 -0400

    added random

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -4,3 +4,7 @@ fetch('monkeytype/1-1000.txt')
     .then(data => wordlibrary = data);
 
 document.getElementById("words").innerHTML = wordlibrary;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
