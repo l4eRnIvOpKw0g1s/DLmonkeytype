16bd57140f26219c5e035bb86eceb3371efabd1f
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-07 12:53:44 -0400

    fixed mistake

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var text = fs.readFileSync("monkeytype/1-1000.txt", 'utf-8');
 var textByLine = text.split('\n');
-console.log (typof textByLine);
+console.log (typof (textByLine));
 console.log (textByLine);
 
 function getRndInteger(min, max) {
