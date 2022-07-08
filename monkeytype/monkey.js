b1e812cfab133c4fec82649df45b7a14aa69ed06
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-08 03:00:02 -0400

    modified

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -1,13 +1,11 @@
-var wordlibrary;
-fetch('monkeytype/1-1000.txt')
-    .then(response => response.text())
-    .then(data => wordlibrary = data.split('\n'));
-
-console.log(typeof wordlibrary);
-console.log(wordlibrary);
-document.getElementById("words").innerHTML = wordlibrary;
-
-
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
+
+async function fetchLibrary() {
+    const result = await fetch('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
+    const data = await result.text()
+    const wordLibrary = await data.split('\n');
+    document.getElementById("words").innerHTML = wordLibrary[getRndInteger(0, wordLibrary.length-1)];
+}
+fetchLibrary();
