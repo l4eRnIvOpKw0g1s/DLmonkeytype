dcbe927ef4a4f87e3bfd4cfcd5b414e528875a94
l4eRnIvOpKw0g1s <davidlim0412@gmail.com>
2022-07-08 17:16:48 -0400

    hello

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -6,6 +6,10 @@ async function fetchLibrary() {
     const result = await fetch('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
     const data = await result.text()
     const wordLibrary = await data.split('\n');
-    document.getElementById("words").innerHTML = wordLibrary[getRndInteger(0, wordLibrary.length-1)];
+    for (let i = 0; i < 25; i++) {
+        typelist.append(wordLibrary[getRndInteger(0, wordLibrary.length-1)])
+    }
+    document.getElementById("words").innerHTML = typelist;
 }
+let typelist
 fetchLibrary();
