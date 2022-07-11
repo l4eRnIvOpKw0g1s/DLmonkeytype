3fef36617640454ff006a20e9686a2696ab0fd00
l4eRnIvOpKw0g1s <105694750+l4eRnIvOpKw0g1s@users.noreply.github.com>
2022-07-11 01:35:07 -0400

    Add files via upload

--- monkeytype/monkey.js
+++ monkeytype/monkey.js
@@ -16,7 +16,7 @@ function getRndInteger(min, max) {
 };
 
 async function fetchLibrary() {
-    const result = await fetch('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt');
+    const result = await fetch('monkeytype/1-1000.txt');
     const data = await result.text();
     const wordLibrary = await data.split('\n');
     let wordlistasync = [];
